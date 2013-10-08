d176bff5157250c66c654f9c6f142d9e2306ca88
Sebastien Duthil <sduthil@avencall.com>
2013-10-08 11:01:56 -0400

    add LineSCCP

--- xivo-dao/xivo_dao/data_handler/line/model.py
+++ xivo-dao/xivo_dao/data_handler/line/model.py
@@ -18,6 +18,7 @@
 from xivo_dao.helpers.abstract_model import AbstractModels
 from xivo_dao.alchemy.linefeatures import LineFeatures as LineSchema
 from xivo_dao.alchemy.usersip import UserSIP as UserSIPSchema
+from xivo_dao.alchemy.sccpline import SCCPLine as SCCPLineSchema
 from xivo_dao.converters.database_converter import DatabaseConverter
 
 
@@ -55,31 +56,44 @@ class LineSIP(AbstractModels):
         self.protocol = 'sip'
 
 
+class LineSCCP(AbstractModels):
+
+    MANDATORY = [
+        'context',
+        'protocol',
+        'device_slot'
+    ]
+
+    FIELDS = [
+        'id',
+        'number',
+        'context',
+        'protocol',
+        'protocolid',
+        'callerid',
+        'device',
+        'provisioning_extension',
+        'configregistrar',
+        'device_slot',
+    ]
+
+    _RELATION = {}
+
+    @property
+    def interface(self):
+        return '%s/%s' % (self.protocol.upper(), self.name)
+
+    def __init__(self, *args, **kwargs):
+        AbstractModels.__init__(self, *args, **kwargs)
+        self.protocol = 'sccp'
+
+
 class LineOrdering(object):
         name = LineSchema.name
         context = LineSchema.context
 
 
 class LineSIPDBConverter(object):
-
-    def __init__(self):
-        self.line_converter = DatabaseConverter(self.LINE_TO_MODEL, LineSchema, LineSIP)
-        self.protocol_converter = DatabaseConverter(self.PROTOCOL_TO_MODEL, UserSIPSchema, LineSIP)
-
-    def to_model(self, line_row, protocol_row):
-        model = self.line_converter.to_model(line_row)
-        self.protocol_converter.update_model(model, protocol_row)
-        return model
-
-    def to_source(self, model):
-        line_row = self.line_converter.to_source(model)
-        line_row.protocol = 'sip'
-        protocol_row = self.protocol_converter.to_source(model)
-        protocol_row.username = ''
-        protocol_row.type = 'friend'
-        protocol_row.category = 'user'
-
-        return line_row, protocol_row
 
     LINE_TO_MODEL = {
         'id': 'id',
@@ -101,6 +115,30 @@ class LineSIPDBConverter(object):
         'name': 'username',
         'secret': 'secret'
     }
+
+    def __init__(self):
+        self.line_converter = DatabaseConverter(self.LINE_TO_MODEL, LineSchema, LineSIP)
+        self.protocol_converter = DatabaseConverter(self.PROTOCOL_TO_MODEL, UserSIPSchema, LineSIP)
+
+    def to_model(self, line_row, protocol_row):
+        model = self.line_converter.to_model(line_row)
+        self.protocol_converter.update_model(model, protocol_row)
+        return model
+
+    def to_source(self, model):
+        line_row = self.line_converter.to_source(model)
+        line_row.protocol = 'sip'
+        protocol_row = self.protocol_converter.to_source(model)
+        protocol_row.username = ''
+        protocol_row.type = 'friend'
+        protocol_row.category = 'user'
+
+        return line_row, protocol_row
+
+    def update_source(self, line_row, protocol_row, model):
+        self.line_converter.update_source(line_row, model)
+        self.protocol_converter.update_source(protocol_row, model)
+
 
     def update_source(self, line_row, protocol_row, model):
         self.line_converter.update_source(line_row, model)
